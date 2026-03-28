deb0a8fef0176cb998ebc130f17fbf244612b949
yosikeller <yosi.keller@gmail.com>
2026-03-28 12:11:36 +0300

    fix: remove BrowserRouter basename for root domain deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/prof-cv-builder/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
